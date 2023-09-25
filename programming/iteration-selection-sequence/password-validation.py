83d855a45bf06458ce2dfabc1941463a75165bf2
Leo Wilkin <leo.wilkin10@gmail.com>
2023-09-25 10:25:37 +0100

    Create password-validation.py

---
+++ programming/iteration-selection-sequence/password-validation.py
@@ -0,0 +1,6 @@
+password = input("Please enter your password: ")
+while password !=  "1234":
+  print("Password incorrect - try again.")
+  password = input("Please enter your password: ")
+else:
+  print("Correct Password, Welcome.")
